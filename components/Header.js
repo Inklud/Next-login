41b2275288ccbc04dca51e6055e3e4fcd5461544
Eilif Johansen <eilif@lykkesdigitalt.no>
2021-07-11 20:33:06 +0200

    Fixed menu on mobile

--- components/Header.js
+++ components/Header.js
@@ -116,40 +116,39 @@ export default function Header(props) {
           {(ref) => (
             <div className="md:hidden" id="mobile-menu">
               <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a
-                  href="#"
-                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Dashboard
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Team
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Projects
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Calendar
-                </a>
-
-                <a
-                  href="#"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-                >
-                  Reports
-                </a>
+                {!user ? (
+                  <>
+                    <Link href="/login">
+                      <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Login
+                      </a>
+                    </Link>
+                    <Link href="/register">
+                      <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Register
+                      </a>
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <Link href="/members">
+                      <a className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+                        Members
+                      </a>
+                    </Link>
+                    <Link href="/">
+                      <a
+                        className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                        onClick={() => {
+                          logout();
+                          setUser(null);
+                        }}
+                      >
+                        Logout
+                      </a>
+                    </Link>
+                  </>
+                )}
               </div>
             </div>
           )}
