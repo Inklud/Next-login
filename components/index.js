0ca93265821ab1d113dc4a5ee2d6ce4a5faff908
Eilif Johansen <eilifjohansen@outlook.com>
2021-07-28 14:04:11 +0200

    Removed capital letter from folders

--- components/index.js
+++ components/index.js
@@ -1,10 +1,10 @@
 export { default as Header } from "./header";
-export { default as Footer } from "./Footer";
-export { default as Loading } from "./Loading";
-export { default as HeaderTitle } from "./HeaderTitle";
-export { default as IndexPage } from "./IndexPage";
-export { default as LoadingHeader } from "./LoadingHeader";
-export { default as ExportProfile } from "./ExportProfile";
+export { default as Footer } from "./footer";
+export { default as Loading } from "./loading";
+export { default as HeaderTitle } from "./headerTitle";
+export { default as IndexPage } from "./indexPage";
+export { default as LoadingHeader } from "./loadingHeader";
+export { default as ExportProfile } from "./exportProfile";
 export { default as LoginForm } from "./auth/Loginform";
 export { default as RegisterForm } from "./auth/Registerform";
 
